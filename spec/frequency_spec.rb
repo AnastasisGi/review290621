5c2c61744d91a4e2918a85521ed06cf99e86ba56
AnastasisGi <anastasiosgks@gmail.com>
2021-06-29 12:31:22 +0100

    test created for ammending higher and lower frrequency together

--- spec/frequency_spec.rb
+++ spec/frequency_spec.rb
@@ -12,6 +12,13 @@ describe Frequency do
     it 'Can ammend the highest frequency' do
         expect(freq_data.format([20,30,60], 10 , 50)).to eq([20,30,50])
     end
+
+
+    it 'Can ammend the highest and lowest  frequency' do
+        expect(freq_data.format([6,30,60,90], 10 , 85)).to eq([10,30,60,85])
+    end
+
+
 end
 
 
